a4d2b097f8ecc2c5a02f0115897534de3f8109de
Humpo <acomerford@albany.edu>
2014-04-21 12:52:21 -0400

    Update digit_sum.py
    added repeat option along with a small fix

--- digit_sum.py
+++ digit_sum.py
@@ -1,19 +1,36 @@
-num_list = []
-marker_0 = 0 
+marker_0 = 0
 while marker_0 == 0:
-	n = input("Type a multi-digit number:  ")    
-	if not n.isdigit():        
-		print ('*Enter valid input')
-	else:  
+	num_list = []
+	marker_1 = 0 
+	while marker_1 == 0:
+		n = input("Type a multi-digit number, and i'll calculate its digit-sum:  ")    
+		if not n.isdigit():        
+			print ('*Enter valid input')
+		else:  
+			break
+	#^Checks if digit
+	j = 0
+	n = str(n)
+	print ('calculating sum...')
+	for i in n:
+	    num_list.append(i)
+	for i in num_list:
+	    i = int(i)
+	    j = i + j
+	print (j)
+####################################################################################################################	
+	marker_2 = 0
+	marker_3 = 0
+	while marker_2 == 0:
+		repeat = input("Would you like to find another integers digit-sum? (y/n) ")    
+		if repeat == "y" or repeat == "yes":
+			break
+		elif repeat == "n" or repeat == "no":
+			marker_3 = 1
+			break
+		else:
+			print ("*Enter valid input")
+	if marker_3 == 1:
+		print ("Closing...")
 		break
-#^Checks if digit
-j = 0
-n = str(n)
-print ('calculating sum...')
-for i in n:
-    num_list.append(i)
-for i in num_list:
-    i = int(i)
-    j = i + j
-print (j)
 #turns input to string and puts each digit into a list and sums the list
